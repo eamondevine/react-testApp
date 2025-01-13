88046a7e65aa4f0a8d4294e081809902f7d215bd
eamondevine <eamondevine@gmail.com>
2025-01-13 08:46:24 +0800

    some styles added, changed the css file.

---
+++ src/CenteredTextMod.tsx
@@ -0,0 +1,5 @@
+function CenteredText() {
+    return <h1>This text should be centered.</h1>;
+}
+
+export default CenteredText
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,15 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item font-effect-shadow-multiple">An item</li>
+      <li className="list-group-item font-effect-fire-animation">
+        A second item
+      </li>
+      <li className="list-group-item font-effect-3d">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
