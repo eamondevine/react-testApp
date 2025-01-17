1fe724320d5896fc5c36a32e6c57e3bc1c030bab
eamondevine <eamondevine@gmail.com>
2025-01-17 15:36:27 +0800

    Added some error catch measures.

--- src/components/BlogsPage.tsx
+++ src/components/BlogsPage.tsx
@@ -5,20 +5,31 @@ import BlogLoader from "./BlogLoader";
 const BlogsPage = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        console.log(res);
+        if (!res.ok) {
+          throw Error("Couldn't fetch the data");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
         setIsPending(false);
       });
   }, []);
 
   return (
     <>
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogLoader blogs={blogs} />}
     </>
